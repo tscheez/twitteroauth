c71aa57e0e1f82bbc5ba30e185822355b1e8f382
Jake <tscheez@gmail.com>
2018-01-16 15:34:00 -0600

    Shared flag

--- src/TwitterOAuth.php
+++ src/TwitterOAuth.php
@@ -348,8 +348,8 @@ class TwitterOAuth extends Config
         if (isset($parameters['media_category'])) {
             $return['media_category'] = $parameters['media_category'];
         }
-        if (isset($parameters['media_category'])) {
-            $return['media_category'] = $parameters['media_category'];
+        if (isset($parameters['shared'])) {
+            $return['shared'] = $parameters['shared'];
         }
         return $return;
     }
